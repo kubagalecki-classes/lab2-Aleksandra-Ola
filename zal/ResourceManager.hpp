f4520a50f1dd3f9bb954d1ec218ff1c90f215bed
Aleksandra-Ola <73643879+Aleksandra-Ola@users.noreply.github.com>
2020-11-10 18:48:30 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -5,4 +5,33 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+     Resource* data;
+
+public:
+    ResourceManager() { data = new Resource; }
+    ~ResourceManager() { delete data; }
+    double get() { return data->get(); }
+    ResourceManager(const ResourceManager& x) { data = new Resource{*x.data}; }
+    ResourceManager& operator=(const ResourceManager& x)
+    {
+        if (this != &x) {
+            delete data;
+            data = new Resource{*x.data};
+        }
+        return *this;
+    }
+    ResourceManager(const ResourceManager&& x)
+    {
+        data   = x.data;
+        x.data = nullptr;
+    }
+    ResourceManager& operator=(const ResourceManager&& x)
+    {
+        if (this != &x) {
+            delete data;
+            data   = x.data;
+            x.data = nullptr;
+        }
+        return *this;
+    }
 };
